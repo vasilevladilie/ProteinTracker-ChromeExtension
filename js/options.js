ad990910b2fc032d9714778a345c4c74534a5b25
skobraf <skobraf@gmail.com>
2018-03-08 12:50:32 +0100

    ES5 to ES6

--- js/options.js
+++ js/options.js
@@ -1,35 +1,35 @@
 
-var pr = document.getElementById('prompt');
-var p = prompt('Enter your name here please: ');
-pr.value = p;
+const  pr   =   document.getElementById('prompt');
+const  p    =   prompt('Enter your name here please: ');
+pr.value    =   p;
 
 
 
 
-$(function () {
-    chrome.storage.sync.get('goal', function (items) {
+$(() => {
+    chrome.storage.sync.get('goal', (items) => {
         $('#goal').val(items.goal);
     });
 
-    $('#save').click(function () {
-        var goal = $('#goal').val();
+    $('#save').click( () => {
+        const goal = $('#goal').val();
         if (goal) {
-            chrome.storage.sync.set({ 'goal': goal }, function () {
+            chrome.storage.sync.set({ 'goal': goal }, () => {
                 close();
             });
         }
     });
 
     $('#reset').click(function () {
-        chrome.storage.sync.set({ 'total' : 0, 'goal' : 0 }, function () {
-            var opt = {
+        chrome.storage.sync.set({ 'total' : 0, 'goal' : 0 }, () => {
+            const opt = {
                 type: "basic",
                 title: "Total reset!",
                 message: "Total & Goal has been reset back to 0!",
                 iconUrl: "img/icon2.png"
             }
 
-            chrome.notifications.create('reset', opt, function () { });
+            chrome.notifications.create('reset', opt,  () => { });
         });
     });
 });
